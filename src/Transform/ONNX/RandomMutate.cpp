2ebb8d96ba753e8e66fb1c55afed23565a3635a1
Ben Limpanukorn <ben@limpanu.com>
2023-03-17 00:32:42 -0700

    add custom mutation pattern driver

--- src/Transform/ONNX/RandomMutate.cpp
+++ src/Transform/ONNX/RandomMutate.cpp
@@ -3,81 +3,116 @@
 #include "mlir/Dialect/Func/IR/FuncOps.h"
 #include "mlir/IR/Builders.h"
 #include "mlir/IR/BuiltinDialect.h"
+#include "mlir/IR/MLIRContext.h"
 #include "mlir/IR/PatternMatch.h"
-#include "mlir/Transforms/DialectConversion.h"
 #include "mlir/Pass/Pass.h"
+#include "mlir/Rewrite/PatternApplicator.h"
+#include "mlir/Support/LLVM.h"
+#include "mlir/Support/LogicalResult.h"
+#include "mlir/Transforms/DialectConversion.h"
 #include "src/Dialect/ONNX/ONNXOps.hpp"
 #include "src/Dialect/ONNX/ONNXOps/OpHelper.hpp"
 #include "src/Pass/Passes.hpp"
 #include "src/Transform/ONNX/ConvOpt.h"
 #include "src/Transform/ONNX/ToyMutators.h"
+#include "llvm/ADT/ArrayRef.h"
 
 using namespace mlir;
 
 namespace {
 struct RandomMutatePass
     : public PassWrapper<RandomMutatePass, OperationPass<func::FuncOp>> {
-    MLIR_DEFINE_EXPLICIT_INTERNAL_INLINE_TYPE_ID(RandomMutatePass)
-    ::llvm::StringRef getArgument() const override { return "mutate-random"; }
+  MLIR_DEFINE_EXPLICIT_INTERNAL_INLINE_TYPE_ID(RandomMutatePass)
+  ::llvm::StringRef getArgument() const override { return "mutate-random"; }
 
-    ::llvm::StringRef getDescription() const override {
-        return "Randomly select operations in ONNX IR mutatte to trigger various transformations.";
-    }
+  ::llvm::StringRef getDescription() const override {
+    return "Randomly select operations in ONNX IR mutatte to trigger various "
+           "transformations.";
+  }
 
-    void runOnOperation() final;
+  void runOnOperation() final;
 };
 void RandomMutatePass::runOnOperation() {
-    ConversionTarget target(getContext());
+  ConversionTarget target(getContext());
 
-    target.addLegalDialect<ONNXDialect, BuiltinDialect, arith::ArithDialect, func::FuncDialect>();
+  target.addLegalDialect<ONNXDialect, BuiltinDialect, arith::ArithDialect,
+      func::FuncDialect>();
 
-    // TODO get list of operations in the input file
-    target.addDynamicallyLegalOp<ONNXDivOp>([&](ONNXDivOp op) {
-        // TODO return false with some probability
-        return false;
-    });
+  // TODO get list of operations in the input file
+  target.addDynamicallyLegalOp<ONNXDivOp>([&](ONNXDivOp op) {
+    // TODO return false with some probability
+    return false;
+  });
 
-    RewritePatternSet patterns(&getContext());
-    patterns.add<toy_mutators::TriggerAddToMulPattern>(&getContext());
+  RewritePatternSet patterns(&getContext());
+  patterns.add<toy_mutators::TriggerAddToMulPattern>(&getContext());
 
-    if (failed(applyPartialConversion(getOperation(), target, std::move(patterns))))
-        signalPassFailure();
+  if (failed(
+          applyPartialConversion(getOperation(), target, std::move(patterns))))
+    signalPassFailure();
 }
 
+const RewritePattern *choosePattern(
+    llvm::SmallVector<const RewritePattern *> patterns) {
+  return patterns[0];
+}
+
+class MutationPatternRewriteDriver : public PatternRewriter {
+public:
+  explicit MutationPatternRewriteDriver(
+      MLIRContext *ctx, const FrozenRewritePatternSet &patterns, Operation *op)
+      : PatternRewriter(ctx), patterns(patterns), matcher(patterns), op(op),
+        context(ctx) {}
+
+  LogicalResult mutate() {
+    op->walk([&](mlir::Operation *op) {
+      // get the set of available mutators for this operation
+      llvm::SmallVector<const RewritePattern *> matchedPatterns;
+      for (const auto &it : patterns.getOpSpecificNativePatterns()) {
+        if (op->getName() == it.first) {
+          for (const RewritePattern *pattern : it.second) {
+            if (succeeded(pattern->match(op))) {
+              matchedPatterns.push_back(pattern);
+            }
+          }
+        }
+      }
+      for (const auto &pattern : patterns.getMatchAnyOpNativePatterns()) {
+        if (succeeded(pattern.match(op))) {
+          matchedPatterns.push_back(&pattern);
+        }
+      }
+      auto pattern = choosePattern(matchedPatterns);
+      pattern->rewrite(op, *this);
+    });
+  }
+
+private:
+  const FrozenRewritePatternSet &patterns;
+  PatternApplicator matcher;
+  Operation *op;
+  MLIRContext *context;
+};
 struct WalkerMutatePass
     : public PassWrapper<WalkerMutatePass, OperationPass<func::FuncOp>> {
-    MLIR_DEFINE_EXPLICIT_INTERNAL_INLINE_TYPE_ID(WalkerMutatePass)
-    void runOnOperation() override {
-        // TODO choose from a set of multiple mutator patterns
-        //RewritePatternSet patterns(&getContext());
-        //patterns.add<toy_mutators::TriggerAddToMulPattern>(&getContext());
-        toy_mutators::TriggerAddToMulPattern pattern(&getContext());
-        getOperation()->walk([&](mlir::Operation *op) {
-            
-            // get the set of available mutators for this operation
-            // TODO: for pattern in patterns:
-            if (succeeded(pattern.match(op))) {
-                // matchedPatterns.add(pattern)
-            }
+  MLIR_DEFINE_EXPLICIT_INTERNAL_INLINE_TYPE_ID(WalkerMutatePass)
+  void runOnOperation() override {
+    MLIRContext *context = &getContext();
+    RewritePatternSet patterns(context);
+    patterns.add<toy_mutators::TriggerAddToMulPattern>(context);
 
-            // TODO: choose one of the patterns based on a heuristic
-            // TODO: pattern = choosePattern(matchedPatterns)
-
-            // apply the chosen pattern
-            OpBuilder opBuilder(op);
-            PatternRewriter rewriter(getContext());
-            pattern.rewrite(op, opBuilder);
-        });
-    }
+    MutationPatternRewriteDriver driver(
+        context, std::move(patterns), getOperation());
+  }
 };
-}
+} // namespace
 
 namespace onnx_mlir {
 std::unique_ptr<mlir::Pass> createRandomMutatePass() {
-    return std::make_unique<RandomMutatePass>();
+  return std::make_unique<RandomMutatePass>();
 }
 std::unique_ptr<mlir::Pass> createWalkerMutatePass() {
-    return std::make_unique<WalkerMutatePass>();
+  return std::make_unique<WalkerMutatePass>();
 }
 
-}
+} // namespace onnx_mlir
